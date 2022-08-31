68ed1d16a5dcee2675d80fc736510760b8b986aa
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-31 00:45:29 -0300

    Criando função para ordenar por id e por nome

--- eCommerce.js
+++ eCommerce.js
@@ -5,12 +5,16 @@ var avaliacao = []
 contador = 0
 
 var continuar = true
+
 while(continuar){
+
 CadastrarProduto()
-console.log(id)
-console.log(nome)
-console.log(preco)
-console.log(avaliacao)
+
+console.log("ID:", id)
+console.log("Nome: ", nome)
+console.log("Preço: ", preco)
+console.log("Avaliação: ", avaliacao)
+
 }
 
 var buscarID = parseInt(prompt(`Qual ID deseja buscar`))
@@ -18,6 +22,13 @@ BuscarProdutoID(buscarID)
 
 var produto = prompt(`Qual produto quer buscar?`)
 BuscarProdutoNome(produto)
+
+OrdenarPorID()
+console.log("ID:", id)
+
+
+OrdenarPorPreco()
+console.log("Preço: ", preco)
 
 
 
@@ -48,5 +59,46 @@ function BuscarProdutoNome(produtoParametro){
             console.log(`ID do produto buscado: ${id[i]}`)
         }
     }
-    
+}
+
+function OrdenarPorID(){
+    for(var atual = 0; atual < contador - 1; atual++){
+        for(var seguinte = atual + 1; seguinte < contador; seguinte++ ){
+            idAux = id[atual]
+            nomeAux = nome[atual]
+            precoAux = preco[atual]
+            avaliacaoAux = avaliacao[atual]
+            if(id[atual] > id[seguinte]){
+                id[atual] = id[seguinte]
+                nome[atual] = nome[seguinte]
+                preco[atual] = preco[seguinte]
+                avaliacao[atual] = avaliacao[seguinte]
+                id[seguinte] = idAux
+                nome[seguinte] = nomeAux
+                preco[seguinte] = precoAux
+                avaliacao[seguinte] = avaliacaoAux
+            }
+        }
+    }
+}
+
+function OrdenarPorPreco(){
+    for(var atual = 0; atual < contador - 1; atual++){
+        for(var seguinte = atual + 1; seguinte < contador; seguinte++ ){
+            idAux = id[atual]
+            nomeAux = nome[atual]
+            precoAux = preco[atual]
+            avaliacaoAux = avaliacao[atual]
+            if(preco[atual] < preco[seguinte]){
+                id[atual] = id[seguinte]
+                nome[atual] = nome[seguinte]
+                preco[atual] = preco[seguinte]
+                avaliacao[atual] = avaliacao[seguinte]
+                id[seguinte] = idAux
+                nome[seguinte] = nomeAux
+                preco[seguinte] = precoAux
+                avaliacao[seguinte] = avaliacaoAux
+            }
+        }
+    }
 }
